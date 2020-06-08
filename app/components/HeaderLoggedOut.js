6dca28eb116e8f2ece0d4f293ad774c58e836c4c
Alpino <alppkoseoglu@gmail.com>
2020-06-08 14:40:50 +0300

    Login fields empty status red border.

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -7,17 +7,26 @@ function HeaderLoggedOut(props) {
 
   const [username, setUsername] = useState()
   const [password, setPassword] = useState()
+  const [usernameEmpty, setUsernameEmpty] = useState(false)
+  const [passwordEmpty, setPasswordEmpty] = useState(false)
 
   async function handleSubmit(e) {
     e.preventDefault()
+    setUsernameEmpty(Boolean(!username))
+    setPasswordEmpty(Boolean(!password))
     try {
-      const response = await Axios.post("/login", { username, password })
-      if (response.data) {
-        appDispatch({ type: "login", data: response.data })
-        appDispatch({ type: "flashMessage", value: "You have successfully logged in." })
+      if (!username.trim() || !password.trim()) {
+        setUsernameEmpty(Boolean(!username.trim()))
+        setPasswordEmpty(Boolean(!password.trim()))
       } else {
-        console.log("Incorrect username / password!")
-        appDispatch({ type: "flashMessage", value: "Invalid username / password." })
+        const response = await Axios.post("/login", { username, password })
+        if (response.data) {
+          appDispatch({ type: "login", data: response.data })
+          appDispatch({ type: "flashMessage", value: "You have successfully logged in." })
+        } else {
+          console.log("Incorrect username / password!")
+          appDispatch({ type: "flashMessage", value: "Invalid username / password." })
+        }
       }
     } catch (e) {
       console.log("There was a problem!")
@@ -28,10 +37,29 @@ function HeaderLoggedOut(props) {
     <form onSubmit={handleSubmit} className="mb-0 pt-2 pt-md-0">
       <div className="row align-items-center">
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input onChange={(e) => setUsername(e.target.value)} name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
+          <input
+            onChange={(e) => {
+              setUsername(e.target.value)
+              setUsernameEmpty(Boolean(!e.target.value))
+            }}
+            name="username"
+            className={"form-control form-control-sm input-dark " + (usernameEmpty ? "is-invalid" : "")}
+            type="text"
+            placeholder="Username"
+            autoComplete="off"
+          />
         </div>
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input onChange={(e) => setPassword(e.target.value)} name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value)
+              setPasswordEmpty(Boolean(!e.target.value))
+            }}
+            name="password"
+            className={"form-control form-control-sm input-dark " + (passwordEmpty ? "is-invalid" : "")}
+            type="password"
+            placeholder="Password"
+          />
         </div>
         <div className="col-md-auto">
           <button className="btn btn-success btn-sm">Sign In</button>
