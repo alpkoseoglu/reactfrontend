d4956b12cb9d35ae92a294f3951a14a228aa0205
Alpino <alppkoseoglu@gmail.com>
2020-06-09 19:58:49 +0300

    Header color and brand adjustment.

--- app/components/Header.js
+++ app/components/Header.js
@@ -9,11 +9,11 @@ function Header(props) {
   const headerContent = appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />
 
   return (
-    <header className="header-bar bg-primary mb-3">
+    <header className="header-bar bg-info mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
         <h4 className="my-0 mr-md-auto font-weight-normal">
           <Link to="/" className="text-white">
-            AlpinoMedia
+            &#60; &#62; AlpinoMedia
           </Link>
         </h4>
         {!props.staticEmpty ? headerContent : ""}
